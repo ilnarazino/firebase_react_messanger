3c2dac527968288798602aa020f0644634586287
ilnar <ilnarazino@yandex.ru>
2022-07-29 00:25:39 +0300

    NavBar

--- src/components/Loginjs.js
+++ src/components/Loginjs.js
@@ -1,10 +1,16 @@
+import { Container, Grid } from "@mui/material";
 import React from "react";
 
-const Loginjs = ()=>{
+const Loginjs = () => {
     return (
-        <div>
-            login
-        </div>
+        <Container>
+            <Grid container
+                style={{ height: window.innerHeight - 50 }}
+                alignItems={"center"}
+                justify={"center"}>
+                    sdfsfdsdf
+            </Grid>
+        </Container>
 
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,20 @@
 import { AppBar, Grid, Toolbar, Button } from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { LOGIN_ROUTE } from "../utils/consts";
 
 const Navbar = () => {
     const user = true
     return (
         <AppBar color={"secondary"} position="static">
-            <Toolbar>
+            <Toolbar variant="dense">
                 <Grid container justify={"flex-end"}>
                     {user ?
                         <Button variant={"outlined"}>Выход</Button>
                         :
-                        <Button variant={"outlined"}>Логин</Button>
+                        <NavLink to={LOGIN_ROUTE}>
+                            <Button variant={"outlined"}>Логин</Button>
+                        </NavLink>
                     }
                 </Grid>
             </Toolbar>
